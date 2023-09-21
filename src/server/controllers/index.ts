5c9495d5a770f998bd281a7e4e81e4c20663a542
Chris Leveille <cdleveille@gmail.com>
2023-09-21 12:16:10 -0400

    minor fixes

--- src/server/controllers/index.ts
+++ src/server/controllers/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 
 import { initUserRoutes, initWsRoutes } from "@controllers";
 
 export const initRoutes = (app: Hono) => {
+	app.use("/*", serveStatic({ root: "public" }));
 	initUserRoutes(app);
 	initWsRoutes(app);
 };
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,5 @@
 import { Hono } from "hono";
-import { serveStatic } from "hono/bun";
-import { Socket } from "socket.io";
+import { Server, Socket } from "socket.io";
 
 import { initRoutes } from "@controllers";
 import { Config } from "@helpers";
@@ -8,20 +7,16 @@ import { initPlugins } from "@plugins";
 import { buildClient } from "@scripts";
 import { connectToDatabase, log } from "@services";
 
-const io = require("socket.io")(Config.WS_PORT, {
-	cors: { origin: `${Config.HOST}:${Config.PORT}` }
-});
-
 await Promise.all([buildClient(), connectToDatabase()]);
 
 log.info(`Starting server in ${Config.IS_PROD ? "production" : "development"} mode...`);
 
 const app = new Hono();
-app.use("/*", serveStatic({ root: "public" }));
-
 initPlugins(app);
 initRoutes(app);
 
+// @ts-ignore
+const io = new Server(Config.WS_PORT, { cors: { origin: `${Config.HOST}:${Config.PORT}` } });
 io.on("connect", (socket: Socket) => {
 	socket.on("hello", () => {
 		log.info("hello from client!");
