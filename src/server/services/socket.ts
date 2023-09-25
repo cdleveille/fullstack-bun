1984b831bbaf34cdde5d7e5d890586be940f2993
Chris Leveille <cdleveille@gmail.com>
2023-09-25 13:36:03 -0400

    project config tweaks

--- src/server/services/socket.ts
+++ src/server/services/socket.ts
@@ -1,13 +1,13 @@
-import { Server } from "socket.io";
+import { Server, ServerOptions } from "socket.io";
 
 import { Config } from "@helpers";
 import { log } from "@services";
 
 export const initSocket = () => {
 	const io = new Server(Config.WS_PORT, {
-		// @ts-ignore
-		cors: { origin: [Config.HOST, `${Config.HOST}:${Config.PORT}`] }
-	});
+		cors: { origin: [Config.HOST, `${Config.HOST}:${Config.PORT}`] },
+		serveClient: false
+	} as Partial<ServerOptions>);
 	io.on("connect", socket => {
 		socket.on("hello", () => {
 			log.info("socket.io: hello from client!");
