fa0e331751cd4a2c714d3062126810949ce4666e
Chris Leveille <cdleveille@gmail.com>
2023-09-27 00:49:30 -0400

    don't use secureHeaders middleware for non-html resources

--- src/server/middleware/secureHeaders.ts
+++ src/server/middleware/secureHeaders.ts
@@ -3,37 +3,35 @@ import { secureHeaders } from "hono/secure-headers";
 
 import { Config } from "@helpers";
 
-export const useSecureHeaders = (app: Hono) =>
-	app.use(
-		"*",
-		secureHeaders({
-			contentSecurityPolicy: {
-				defaultSrc: ["'self'"],
-				baseUri: ["'self'"],
-				childSrc: ["'self'"],
-				connectSrc: [
-					"'self'",
-					`${Config.HOST}:${Config.WS_PORT}`,
-					`${Config.HOST.replace("http", "ws")}:${Config.WS_PORT}`
-				],
-				fontSrc: ["'self'", "https:", "data:"],
-				formAction: ["'self'"],
-				frameAncestors: ["'self'"],
-				frameSrc: ["'self'"],
-				imgSrc: ["'self'", "data:"],
-				manifestSrc: ["'self'"],
-				mediaSrc: ["'self'"],
-				objectSrc: ["'none'"],
-				scriptSrc: ["'self'"],
-				scriptSrcAttr: ["'none'"],
-				scriptSrcElem: ["'self'"],
-				styleSrc: ["'self'", "https:", "'unsafe-inline'"],
-				styleSrcAttr: ["none"],
-				styleSrcElem: ["'self'", "https:", "'unsafe-inline'"],
-				upgradeInsecureRequests: [],
-				workerSrc: ["'self'"]
-			},
-			xFrameOptions: false,
-			xXssProtection: false
-		})
-	);
+export const useSecureHeaders = (app: Hono) => app.use("/", secureHeadersOptions);
+
+const secureHeadersOptions = secureHeaders({
+	contentSecurityPolicy: {
+		defaultSrc: ["'self'"],
+		baseUri: ["'self'"],
+		childSrc: ["'self'"],
+		connectSrc: [
+			"'self'",
+			`${Config.HOST}:${Config.WS_PORT}`,
+			`${Config.HOST.replace("http", "ws")}:${Config.WS_PORT}`
+		],
+		fontSrc: ["'self'", "https:", "data:"],
+		formAction: ["'self'"],
+		frameAncestors: ["'self'"],
+		frameSrc: ["'self'"],
+		imgSrc: ["'self'", "data:"],
+		manifestSrc: ["'self'"],
+		mediaSrc: ["'self'"],
+		objectSrc: ["'none'"],
+		scriptSrc: ["'self'"],
+		scriptSrcAttr: ["'none'"],
+		scriptSrcElem: ["'self'"],
+		styleSrc: ["'self'", "https:", "'unsafe-inline'"],
+		styleSrcAttr: ["none"],
+		styleSrcElem: ["'self'", "https:", "'unsafe-inline'"],
+		upgradeInsecureRequests: [],
+		workerSrc: ["'self'"]
+	},
+	xFrameOptions: false,
+	xXssProtection: false
+});
