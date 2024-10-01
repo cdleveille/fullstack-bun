393aecce7ba7665dd3747ef8ec65bda04cbfa49d
Chris Leveille <cdleveille@gmail.com>
2024-10-01 13:41:14 -0400

    use established type naming convention

--- src/client/hooks/useApi.ts
+++ src/client/hooks/useApi.ts
@@ -5,21 +5,21 @@ import type { TSocketEvent } from "@types";
 
 const TIMEOUT_MS = 5000;
 
-interface IReqParams<T = unknown> {
+type TReqParams<T = unknown> = {
 	event: TSocketEvent;
 	data?: unknown;
 	callback?: ((res: T) => void) | (() => void);
-}
+};
 
 export const useApi = () => {
 	const { socket } = useSocket();
 
-	const to = ({ event, data, callback }: IReqParams) => {
+	const to = ({ event, data, callback }: TReqParams) => {
 		socket.emit(event, data);
 		callback?.(null);
 	};
 
-	const toAndFrom = async <T>({ event, data, callback }: IReqParams<T>) => {
+	const toAndFrom = async <T>({ event, data, callback }: TReqParams<T>) => {
 		return new Promise<T>((resolve, reject) => {
 			const timeout = setTimeout(() => reject(`Request timed out after ${TIMEOUT_MS}ms.`), TIMEOUT_MS);
 			socket.once(event, res => {
