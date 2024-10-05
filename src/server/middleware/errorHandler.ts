16feaa49b76d698c05e4d7b54a659e24b9cddf73
Chris Leveille <cdleveille@gmail.com>
2024-10-05 11:49:58 -0400

    mark param as unused

--- src/server/middleware/errorHandler.ts
+++ src/server/middleware/errorHandler.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 
 import { CustomError } from "@types";
 
-export const errorHandler = (error: Error | CustomError, req: Request, res: Response, next: NextFunction) => {
+export const errorHandler = (error: Error | CustomError, _req: Request, res: Response, next: NextFunction) => {
 	if (!error) return next();
 	const statusCode = error instanceof CustomError ? error.statusCode : res.statusCode !== 200 ? res.statusCode : 500;
 	const errorMessage = (typeof error === "string" ? error : error.message) || "Internal Server Error";
