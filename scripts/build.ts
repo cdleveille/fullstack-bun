e2bb8f07050203b7214620ff9799ebd0b564fda7
Chris Leveille <cdleveille@gmail.com>
2024-10-16 16:14:32 -0400

    added comments to build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -14,12 +14,14 @@ const isProd = await BunPack.build(bunPackConfig);
 
 const indexHtml = path.join(bunPackConfig.outDir, "index.html");
 const serverFile = path.resolve("./src/server/index.ts");
+const reloadScriptTag = "<script src=/reload/reload.js></script>";
 
 if (isProd) {
-	let indexHtmlContents = await Bun.file(indexHtml).text();
-	indexHtmlContents = indexHtmlContents.replace("<script src=/reload/reload.js></script>", "");
+	// in production, remove unnecessary reload script tag from html
+	const indexHtmlContents = (await Bun.file(indexHtml).text()).replace(reloadScriptTag, "");
 	await Bun.write(indexHtml, indexHtmlContents);
 } else {
+	// in development, save server file to trigger browser refresh via reload package
 	const serverFileContents = await Bun.file(path.resolve(serverFile)).text();
 	await Bun.write(serverFile, serverFileContents);
 }
