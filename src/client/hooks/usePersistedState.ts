9b6ecc4149929355244d0b095e72885702c98640
Chris Leveille <cdleveille@gmail.com>
2024-10-21 18:56:29 -0400

    tweak for usePersistedState hook

--- src/client/hooks/usePersistedState.ts
+++ src/client/hooks/usePersistedState.ts
@@ -1,15 +1,15 @@
 import { useEffect, useMemo, useState } from "react";
 
 import { STORED_STATE_PREFIX } from "@constants";
-import { Config, storage } from "@utils";
+import { storage } from "@utils";
 
 export const usePersistedState = <T>(
 	initialValue: T,
 	id: string,
-	isEnabledInProd = false
+	isPersistDisabled = false
 ): [T, React.Dispatch<React.SetStateAction<T>>] => {
 	const persistedInitialValue = useMemo(() => {
-		if (Config.IS_PROD && !isEnabledInProd) return initialValue;
+		if (isPersistDisabled) return initialValue;
 		const storedValue = storage.session.getItem<T>(`${STORED_STATE_PREFIX}:${id}`);
 		return storedValue ?? initialValue;
 	}, []);
@@ -17,7 +17,7 @@ export const usePersistedState = <T>(
 	const [state, setState] = useState<T>(persistedInitialValue);
 
 	useEffect(() => {
-		if (Config.IS_PROD && !isEnabledInProd) return;
+		if (isPersistDisabled) return;
 		storage.session.setItem(`${STORED_STATE_PREFIX}:${id}`, state);
 	}, [state]);
 
