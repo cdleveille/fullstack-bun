e572ce075e53773a6a47b6cea95e11afe685612e
Chris Leveille <cdleveille@gmail.com>
2024-10-22 02:48:58 -0400

    include stack trace in error handler response in dev

--- src/server/middleware/errorHandler.ts
+++ src/server/middleware/errorHandler.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 
+import { Config } from "@helpers";
 import { CustomError } from "@types";
 
 export const errorHandler = (error: Error | CustomError, _req: Request, res: Response, next: NextFunction) => {
 	if (!error) return next();
 	const statusCode = error instanceof CustomError ? error.statusCode : res.statusCode !== 200 ? res.statusCode : 500;
 	const errorMessage = (typeof error === "string" ? error : error.message) || "Internal Server Error";
-	res.status(statusCode).json({ message: errorMessage });
+	res.status(statusCode).json({ message: errorMessage, stack: !Config.IS_PROD ? error.stack : undefined });
 };
