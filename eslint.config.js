c3bb9c2f5c0b9460f0675c12e2a594eed9b28cc3
Chris Leveille <cdleveille@gmail.com>
2024-10-22 17:24:43 -0400

    additional stylistic linting rules

--- eslint.config.js
+++ eslint.config.js
@@ -6,21 +6,27 @@ export default [
 	{
 		ignores: ["node_modules/", "public/"]
 	},
-	...tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked, {
-		languageOptions: {
-			parserOptions: {
-				projectService: true,
-				tsconfigRootDir: import.meta.dirname
-			}
-		},
-		rules: {
-			"@typescript-eslint/ban-ts-comment": "off",
-			"@typescript-eslint/no-misused-promises": [
-				"error",
-				{
-					checksVoidReturn: false
+	...tseslint.config(
+		eslint.configs.recommended,
+		...tseslint.configs.recommendedTypeChecked,
+		...tseslint.configs.stylisticTypeChecked,
+		{
+			languageOptions: {
+				parserOptions: {
+					projectService: true,
+					tsconfigRootDir: import.meta.dirname
 				}
-			]
+			},
+			rules: {
+				"@typescript-eslint/ban-ts-comment": "off",
+				"@typescript-eslint/consistent-type-definitions": ["error", "type"],
+				"@typescript-eslint/no-misused-promises": [
+					"error",
+					{
+						checksVoidReturn: false
+					}
+				]
+			}
 		}
-	})
+	)
 ];
--- src/server/helpers/config.ts
+++ src/server/helpers/config.ts
@@ -2,9 +2,9 @@ import { Env } from "@constants";
 
 const IS_PROD = Bun.env.BUN_ENV === Env.Production || Bun.env.NODE_ENV === Env.Production;
 
-const PORT = parseInt(Bun.env.PORT || (IS_PROD ? "8080" : "3000"));
+const PORT = parseInt(Bun.env.PORT ?? (IS_PROD ? "8080" : "3000"));
 
-const MONGO_URI = IS_PROD ? Bun.env.MONGO_URI : Bun.env.MONGO_URI || "mongodb://localhost:27017/fullstack-bun";
+const MONGO_URI = IS_PROD ? Bun.env.MONGO_URI : (Bun.env.MONGO_URI ?? "mongodb://localhost:27017/fullstack-bun");
 
 const SKIP_DB = Bun.env.SKIP_DB === "true";
 
