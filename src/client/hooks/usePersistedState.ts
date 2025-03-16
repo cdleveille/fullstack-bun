d700b9f9aecdfa110082a23b68bbfe4dd1848e39
Chris Leveille <cdleveille@gmail.com>
2025-03-16 17:18:47 -0400

    use react state setter type

--- src/client/hooks/usePersistedState.ts
+++ src/client/hooks/usePersistedState.ts
@@ -1,13 +1,14 @@
 import { useEffect, useMemo, useState } from "react";
 
 import { STORED_STATE_PREFIX } from "@constants";
+import type { TReactStateSetter } from "@types";
 import { storage } from "@utils";
 
 export const usePersistedState = <T>(
 	initialValue: T,
 	id: string,
 	isPersistDisabled = false
-): [T, React.Dispatch<React.SetStateAction<T>>] => {
+): [T, TReactStateSetter<T>] => {
 	const persistedInitialValue = useMemo(() => {
 		if (isPersistDisabled) return initialValue;
 		const storedValue = storage.session.getItem<T>(`${STORED_STATE_PREFIX}:${id}`);
