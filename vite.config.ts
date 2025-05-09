5b52171c17d012ab961678f7d97f6f7d3f8ef80d
Chris Leveille <cdleveille@gmail.com>
2025-05-09 12:58:41 -0400

    simplified vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,32 +7,25 @@ import { Env, Path } from "@constants";
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd());
 
-	const port = Number.parseInt(env.VITE_DEV_PORT ?? "5173");
-
-	const target = `http://localhost:${env.VITE_PORT ?? "3000"}`;
+	const PORT = env.VITE_PORT ?? "3000";
 
 	return {
 		plugins: [react(), tsconfigPaths()],
 		root: Path.ClientSrc,
 		define: {
 			"import.meta.env.VITE_ENV": JSON.stringify(env.VITE_ENV ?? Env.Development),
-			"import.meta.env.VITE_PORT": JSON.stringify(env.VITE_PORT)
+			"import.meta.env.VITE_PORT": JSON.stringify(PORT)
 		},
 		server: {
 			open: true,
 			hmr: true,
-			port,
+			port: Number.parseInt(env.VITE_DEV_PORT ?? "5173"),
 			strictPort: false,
 			proxy: {
 				"/api": {
-					target,
+					target: `http://localhost:${PORT}`,
 					changeOrigin: true
 				}
-				// "/socket.io": {
-				// 	target,
-				// 	changeOrigin: true,
-				// 	ws: true
-				// }
 			},
 			fs: {
 				deny: ["sw.*"]
