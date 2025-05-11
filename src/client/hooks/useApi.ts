81de35fb0ab5c8a25b7fc10bd7a594c3c6de672d
Chris Leveille <cdleveille@gmail.com>
2025-05-11 17:07:04 -0400

    disable client queries when offline

--- src/client/hooks/useApi.ts
+++ src/client/hooks/useApi.ts
@@ -1,6 +1,7 @@
 import { treaty } from "@elysiajs/eden";
 import { useQuery } from "@tanstack/react-query";
 
+import { useIsOffline } from "@client/hooks/useIsOffline";
 import { socket } from "@client/utils/socket";
 import type { TApi } from "@server/helpers/api";
 import { SocketEvent } from "@shared/constants";
@@ -8,10 +9,13 @@ import { SocketEvent } from "@shared/constants";
 const client = treaty<TApi>(window.location.origin);
 
 export const useApi = () => {
+	const isOffline = useIsOffline();
+
 	const helloSocket = (message: string) =>
 		useQuery({
 			queryKey: ["hello-socket"],
-			queryFn: () => socket.emitAndReceive({ event: SocketEvent.Hello, data: [message] })
+			queryFn: () => socket.emitAndReceive({ event: SocketEvent.Hello, data: [message] }),
+			enabled: !isOffline
 		});
 
 	const helloHttp = (name?: string) =>
@@ -22,7 +26,8 @@ export const useApi = () => {
 					query: { name }
 				});
 				return data;
-			}
+			},
+			enabled: !isOffline
 		});
 
 	return { helloSocket, helloHttp };
