9a2c0abf71ff764427fc1d0f201ff42f790cc7a4
Chris Leveille <cdleveille@gmail.com>
2025-05-11 01:19:32 -0400

    improve sw registration

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -7,12 +7,9 @@ import { AppContextProvider, ErrorBoundary, Main } from "@components";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Config, assertGetElementById } from "@utils";
 
-window.addEventListener("load", async () => {
-	if (!Config.IS_PROD || !navigator.serviceWorker) return;
-	if (!navigator.serviceWorker.controller) {
-		await navigator.serviceWorker.register("sw.js", { type: "module", scope: "/" });
-	}
-});
+if (Config.IS_PROD && navigator.serviceWorker && !navigator.serviceWorker.controller) {
+	navigator.serviceWorker.register("sw.js", { type: "module", scope: "/" }).catch(console.error);
+}
 
 const root = assertGetElementById("root");
 createRoot(root).render(
