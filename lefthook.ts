f5d63c83a5b3798dd96e717c3c371fcfd2770992
Chris Leveille <cdleveille@gmail.com>
2025-05-11 15:22:10 -0400

    fixed lefthook config

--- lefthook.ts
+++ lefthook.ts
@@ -2,7 +2,7 @@ import { chmod, exists, mkdir, writeFile } from "node:fs/promises";
 import { join } from "node:path";
 
 if (!(await exists(".git"))) {
-	throw new Error("Error: lefthook.ts script must be run from the root folder of git repo");
+	throw new Error("lefthook.ts script must be run from the root folder of git repo");
 }
 
 const HOOKS_DIR = ".git/hooks";
@@ -11,10 +11,10 @@ await mkdir(HOOKS_DIR, { recursive: true });
 
 const preCommit = `#!/bin/sh
 export PATH="$PWD/node_modules/.bin:$PATH"
-lefthook run pre-commit`;
+lefthook run pre-commit --no-auto-install`;
 
 await writeFile(join(HOOKS_DIR, "pre-commit"), preCommit);
 
 await chmod(join(HOOKS_DIR, "pre-commit"), 0o755);
 
-console.log("lefthook hooks installed ✅");
+console.log("lefthook hooks synced ✅");
