329b70f016c7a00df850bc311624ddc5062f2f70
Chris Leveille <cdleveille@gmail.com>
2025-05-11 03:54:21 -0400

    better port env var loading

--- src/server/helpers/config.ts
+++ src/server/helpers/config.ts
@@ -1,6 +1,6 @@
-const PORT = Number.parseInt(process.env.PORT ?? "3000");
+const PORT = Number(process.env.PORT) || 3000;
 
-const DEV_PORT = Number.parseInt(process.env.DEV_PORT ?? "5173");
+const DEV_PORT = Number(process.env.DEV_PORT) || 5173;
 
 const HOST = process.env.HOST ?? "http://localhost";
 
