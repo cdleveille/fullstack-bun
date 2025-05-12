111d4c8afc902e0730c2b6f1b63313a139d69661
Chris Leveille <cdleveille@gmail.com>
2025-05-12 13:47:42 -0400

    add dom to lib array in tsconfig

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,5 +1,3 @@
-/// <reference lib="dom" />
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
